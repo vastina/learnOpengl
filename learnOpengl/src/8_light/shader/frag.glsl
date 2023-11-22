fd3fe5ee498bd6b549f8c1cda18e43b7e8e820e7
vastina <143489139+vastina@users.noreply.github.com>
2023-11-22 19:16:26 +0800

    why?

--- learnOpengl/src/8_light/shader/frag.glsl
+++ learnOpengl/src/8_light/shader/frag.glsl
@@ -6,14 +6,20 @@ in vec3 normal;
 in vec3 fragpos;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform float ambientStrength = 0.1;
+uniform float ambientStrength ;
+uniform float specularStrength = 0.3 ;
 uniform vec3 lightpos = vec3(5.0, 5.0, 5.0); 
 uniform vec4 light_fragcolor = vec4(1.0, 1.0, 1.0, 1.0);
-vec3 norm;	vec3 lightDir;
-void main(){
+uniform vec3 viewpos;
+vec3 norm;	vec3 lightDir;  vec3 viewDir;	vec3 reflectDir;	vec3 specular;
+void main()
+{
 norm = normalize(normal);
 lightDir = normalize(lightpos - fragpos);
 float diff = max(dot(norm, lightDir), 0.0);
-vec4 result = (diff + ambientStrength) * light_fragcolor;
+viewDir = normalize(viewpos - fragpos);
+reflectDir = reflect(-lightDir, norm);  
+float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+vec4 result = (1.0*diff + ambientStrength+ specularStrength * spec) * light_fragcolor;
 frag_color = mix(texture(texture1, texture_coord), texture(texture2, texture_coord), 0.2) * result * vec4(out_color, 1.0);
 }
--- learnOpengl/src/8_light/shader/vertex.glsl
+++ learnOpengl/src/8_light/shader/vertex.glsl
@@ -12,9 +12,9 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 void main(){
-gl_Position =  projection * view * model * trans1 * vec4(_gl_position, 1.0);
+fragpos = vec3(model * trans1 * vec4(_gl_position, 1.0) ); 
+gl_Position =  projection * view * vec4(fragpos, 1.0);
 texture_coord = _texture_coord;
 out_color = _out_color;
-normal =  vec3(model * trans1 * vec4(_normal, 1.0) );
-fragpos = vec3(model * trans1 * vec4(_gl_position, 1.0) );
+normal =  mat3(transpose(inverse(model*trans1))) * _normal ;
 }
